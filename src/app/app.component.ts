44bad381eda2d7ab0963b096824b17b273eb2d62
Aadarsh <aadarsh.yedire@cox.com>
2024-02-06 00:27:26 +0530

    base set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,ProfilePageComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
